f928ff9ecec63c16d2b26153d7aec8af217bd015
Maxiee <maxieewong@gmail.com>
2024-06-27 00:27:32 +0800

    DtoUtils

---
+++ src/utils/DtoUtils.ts
@@ -0,0 +1,31 @@
+import { Schema } from 'mongoose';
+import { IBook } from '../models/Book';
+import { IBookFile } from '../models/BookFile';
+
+export class DtoUtils {
+  static toTransferable<T extends { _id: Schema.Types.ObjectId | string }>(doc: T): T {
+    const result: any = {};
+
+    for (const [key, value] of Object.entries(doc)) {
+      if (value instanceof Schema.Types.ObjectId) {
+        result[key] = value.toString();
+      } else if (Array.isArray(value)) {
+        result[key] = value.map(item => 
+          item instanceof Schema.Types.ObjectId ? item.toString() : item
+        );
+      } else {
+        result[key] = value;
+      }
+    }
+
+    return result as T;
+  }
+
+  static toTransferableBook(book: IBook): IBook {
+    return this.toTransferable(book);
+  }
+
+  static toTransferableBookFile(bookFile: IBookFile): IBookFile {
+    return this.toTransferable(bookFile);
+  }
+}
