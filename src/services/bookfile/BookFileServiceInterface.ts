027a0889def5d9d215dd2538e5f6bcd23700549a
Maxiee <maxieewong@gmail.com>
2024-07-06 14:59:10 +0800

    BookFileService.calculateAndUpdateMd5

--- src/services/bookfile/BookFileServiceInterface.ts
+++ src/services/bookfile/BookFileServiceInterface.ts
@@ -36,4 +36,15 @@ export interface IBookFileService {
    * @returns A promise that resolves to the API response containing the content of the book file as a Buffer, or null if the file is not found.
    */
   getBookFileContent(bookId: Id): Promise<ApiResponse<Buffer | null>>;
+
+  /**
+   * Calculates and updates the MD5 hash of a book file.
+   * @param bookId - The ID of the book.
+   * @param fileId - The ID of the file.
+   * @returns A promise that resolves to the API response containing the updated book file.
+   */
+  calculateAndUpdateMd5(
+    bookId: Id,
+    fileId: Id
+  ): Promise<ApiResponse<IBookFile>>;
 }
