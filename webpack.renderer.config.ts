eda7d00fb9c160250dbe6c04d455d35c44f58f1b
Maxiee <maxieewong@gmail.com>
2024-07-06 13:24:02 +0800

    make deb

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,26 +1,29 @@
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
 
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
-import path from 'path';
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
+import path from "path";
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
 });
 
 export const rendererConfig: Configuration = {
-  target: 'electron-renderer',
+  target: "electron-renderer",
   module: {
     rules,
   },
   plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
     alias: {
-      'react': path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-      'react-reader': path.resolve(__dirname, 'node_modules/react-reader/dist/react-reader.es.js'),
-    }
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+      "react-reader": path.resolve(
+        __dirname,
+        "node_modules/react-reader/dist/react-reader.es.js"
+      ),
+    },
   },
 };
