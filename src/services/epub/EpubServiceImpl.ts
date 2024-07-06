4f90b7051e116c9f9696714eea6df0e614dc8b1b
Maxiee <maxieewong@gmail.com>
2024-07-06 23:33:43 +0800

    修复书记元信息提取失败

--- src/services/epub/EpubServiceImpl.ts
+++ src/services/epub/EpubServiceImpl.ts
@@ -1,11 +1,24 @@
 import EPub from "epub2";
 import { IMetadata } from "epub2/lib/epub/const";
+import { ApiResponse } from "../../core/ipc/ApiResponse";
 
 class EpubService {
-  async extractMetadata(filePath: string): Promise<IMetadata> {
-    const epub: EPub = await EPub.createAsync(filePath);
-    const metadata = epub.metadata;
-    return metadata;
+  async extractMetadata(filePath: string): Promise<ApiResponse<IMetadata>> {
+    try {
+      const epub: EPub = await EPub.createAsync(filePath);
+      const metadata = epub.metadata;
+      return {
+        success: true,
+        message: "Successfully extracted metadata",
+        payload: metadata,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: "Failed to extract metadata",
+        payload: null,
+      };
+    }
   }
 }
 
--- src/services/epub/EpubServiceInterface.ts
+++ src/services/epub/EpubServiceInterface.ts
@@ -1,5 +1,6 @@
 import { IMetadata } from "epub2/lib/epub/const";
+import { ApiResponse } from "../../core/ipc/ApiResponse";
 
 export interface IEpubService {
-  extractMetadata(filePath: string): Promise<IMetadata>;
+  extractMetadata(filePath: string): Promise<ApiResponse<IMetadata>>;
 }
